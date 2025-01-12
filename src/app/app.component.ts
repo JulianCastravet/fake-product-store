a0d4c5bdfccf57c1462ada2d0c68e92db48be816
Anonymous2910 <castravet.iulian@gmail.com>
2025-01-12 17:16:03 +0200

    added productComponent with service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'productStore';
+  title = environment.siteName;
 }
---
+++ src/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  siteName: 'Product Store'
+}
